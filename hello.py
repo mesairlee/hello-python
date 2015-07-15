c88f322b8c861eef9b2f4c567e1dcdb71472e3d4
raptor <raptor@raptors-iMac.fritz.box>
2015-07-15 15:10:29 +0200

    yay added a simple counter

--- hello.py
+++ hello.py
@@ -3,14 +3,16 @@ import uuid
 from flask import Flask
 app = Flask(__name__)
 my_uuid = str(uuid.uuid1())
-BLUE = "#0099FF"
+BLUE = "#CCFF00"
 GREEN = "#33CC33"
+counter = 0
 
 COLOR = BLUE
 
 @app.route('/')
 def hello():
-
+	global counter
+	counter = counter + 1
 	return """
 	<html>
 	<body bgcolor="{}">
@@ -19,9 +21,14 @@ def hello():
 	{}
 	</center>
 
+	<center>
+		<p>
+		Page Hits: {}
+  		</p>
+	</center>
 	</body>
 	</html>
-	""".format(COLOR,my_uuid)
+	""".format(COLOR,my_uuid, counter)
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))
